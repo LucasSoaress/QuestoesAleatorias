4b49607d9bd8bf138fd12b6d9937ba2dedcf6b4a
Lucas Soares <lsoares267@gmail.com>
2016-12-05 22:27:50 -0200

    Upando

---
+++ notas.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite sua primeira nota "))
+n2 = int(input("Digite sua segunda nota "))
+n3 = int(input("Digite sua terceira nota "))
+
+mediaFinal = (n1 * 2 + n2 * 3 + n3 * 5) / 10
+
+print(mediaFinal)
